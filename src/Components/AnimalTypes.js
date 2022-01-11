5cb9a2999669f7a4133d80adc3746e193f537bdd
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-01-11 17:34:02 -0500

    added functionality for animaltypes, working on duplicates

--- src/Components/AnimalTypes.js
+++ src/Components/AnimalTypes.js
@@ -4,19 +4,50 @@ import "./AnimalTypes.css";
 const animalTypes = ["dog", "cat", "ferret", "bird", "fish", "snake", "lizard"];
 
 const AnimalTypes = () => {
+  const [ input, setInput ] = useState("");
+  const [ animals, setAnimals ] = useState(animalTypes);
+
+  console.log(input);
+
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setAnimals([...animals, input]);
+  }
+
+  const handleRemove = () => {
+    
+  }
+
+  const handleAdd = () => {
+    
+  }
+
+  const animalTypesElArr = animals.map((animal)=> {
+    return (
+      <>
+        <li>{animal} </li>
+        <button onClick={handleAdd}>+</button>
+        <button onClick={handleRemove}>-</button>
+      </>
+      );
+  });
 
   return(
       <section className={"animal-types"}>
         <h4>Animal Types</h4>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="type">
-            <input type="text" id="animal-type" />
+            <input type="text" id="animal-type" value={input} onChange={handleInputChange}/>
           </label>
           <input type="submit" />
         </form>
-      <ol></ol>
+      <ol>{animalTypesElArr}</ol>
     </section>
-  )
-}
+  );
+};
 
 export default AnimalTypes;
